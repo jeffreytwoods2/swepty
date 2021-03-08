6783cffa62eabd96933f1b8feb8c105538c2c25f
GnomeSaiyan <69124994+jeffreytwoods2@users.noreply.github.com>
2021-03-07 22:26:34 -0700

    Created html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = function (app) {
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+
+    app.get("/engines", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/engines.html"));
+    });
+
+    app.get("/slant", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/slant-six.html"));
+    });
+
+    app.get("/axles", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/axles.html"));
+    });
+
+    app.get("/classes", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/classes.html"));
+    });
+
+    app.get("/special", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/special.html"));
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"));
+    });
+};
